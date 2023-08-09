b3507090df1e839f2e6e6530572508eedb28b484
sebastianabacum <sebastian@abacum.io>
2023-08-09 11:31:39 +0200

    fix cors

--- vending_machine/settings.py
+++ vending_machine/settings.py
@@ -15,8 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
